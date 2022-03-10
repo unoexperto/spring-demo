2f000df065f1c381900b1c4df2cf3255a8375962
unoexperto <unoexperto@users.noreply.github.com>
2022-03-10 18:59:57 +0000

    Initial commit.

---
+++ HELP.md
@@ -0,0 +1,26 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The JVM level was changed from '11' to '17', review the [JDK Version Range](https://github.com/spring-projects/spring-framework/wiki/Spring-Framework-Versions#jdk-version-range) on the wiki for more details.
+
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Gradle documentation](https://docs.gradle.org)
+* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.0.0-SNAPSHOT/gradle-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.0.0-SNAPSHOT/gradle-plugin/reference/html/#build-image)
+* [Coroutines section of the Spring Framework Documentation](https://docs.spring.io/spring/docs/6.0.0-SNAPSHOT/spring-framework-reference/languages.html#coroutines)
+* [Spring Data JDBC](https://docs.spring.io/spring-data/jdbc/docs/current/reference/html/)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Using Spring Data JDBC](https://github.com/spring-projects/spring-data-examples/tree/master/jdbc/basics)
+
+### Additional Links
+These additional references should also help you:
+
+* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+
---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val springBoot = "2.7.0-SNAPSHOT"
+
+plugins {
+//	id("org.springframework.boot") version "2.7.0-SNAPSHOT"
+//	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+//	kotlin("plugin.spring") version "1.6.10"
+
+    kotlin("jvm") version "1.6.10"
+//	kotlin("kapt") version "1.6.10" // used by dsl-json
+    application //to run JVM part
+    `java-library`
+    idea
+    `kotlin-dsl` //version "2.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib", "1.6.10"))
+    implementation(kotlin("reflect", "1.6.10"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc:$springBoot") {
+        exclude("org.springframework.boot", "spring-boot-starter-logging")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-webflux:$springBoot") {
+        exclude("org.springframework.boot", "spring-boot-starter-logging")
+    }
+//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+//	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation("com.h2database:h2:2.1.210")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBoot")
+//	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+        apiVersion = "1.6"
+        languageVersion = "1.6"
+    }
+}
+
+//tasks.withType<Test> {
+//	useJUnitPlatform()
+//}
+
+tasks {
+    wrapper {
+        gradleVersion = "7.3.1"
+        distributionType = Wrapper.DistributionType.BIN
+    }
+}
+
+application {
+    mainClass.set("com.example.demo.DemoApplication")
+//    mainClassName = "com.bitparticles.EntryPointKt"
+//    mainClassName = "com.bitparticles.engine.EngineKt"
+//    applicationDefaultJvmArgs = listOf("--enable-preview")
+}
